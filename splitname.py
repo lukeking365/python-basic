8e27323ce403b8880c3800c47a50bd96ede17a35
lukeking365 <35659169@qq.com>
2019-01-15 10:29:58 +0800

    Update splitname.py

--- splitname.py
+++ splitname.py
@@ -1,12 +1,11 @@
 firstNameList=["李","王","张","刘","陈","杨","赵","黄","周","吴","徐","孙","胡","朱","高","林","何","郭","司马","司"]
 def getFiristName(name):
-    lenName = len(name)
-    while lenName > 0:
-        if name[0:lenName] in firstNameList:
-            lenFirstName = len(name[0:lenName])
-            print("姓："+ name[0:lenFirstName] +" 名："+ name[lenFirstName:(lenName+2)])
+    lenName = n = len(name)
+    while n > 0:
+        if name[0:n] in firstNameList:
+            print("姓："+ name[0:n] +" 名："+ name[n:(lenName+1)])
             break
-        lenName -=1
+        n -= 1
 #test
 if __name__ == "__main__":
     getFiristName("高晓婷")
